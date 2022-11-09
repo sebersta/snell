83029777d44836afbef0a686160a3811944dd8a7
Chien Chin <flounderqin@outlook.com>
2022-11-09 15:27:00 +0800

    Update snell.sh

--- snell.sh
+++ snell.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 apt-get install wget unzip -y
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
+sysctl net.ipv4.tcp_available_congestion_control
 cd
 wget -c https://dl.nssurge.com/snell/snell-server-v4.0.0-linux-amd64.zip
 unzip -o snell-server-v4.0.0-linux-amd64.zip
@@ -17,6 +21,6 @@ y | ./snell-server
 systemctl enable snell
 systemctl start snell
 echo
-echo "Snell service is set up succesfully. Copy the following line to surge"
+echo "Copy the following line to surge"
 echo "$(curl -s ipinfo.io/city) = snell, $(curl -s ipinfo.io/ip), $(cat snell-server.conf | grep -i listen | cut --delimiter=':' -f2), $(cat snell-server.conf |grep psk | sed 's/ //g'), version=4"
 
