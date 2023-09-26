f729b03813d52f91892e19839ef2a3b1a4b77138
Jian Qin <flounderqin@outlook.com>
2023-09-25 20:21:22 -0700

    Update rmsnell.sh

--- rmsnell.sh
+++ rmsnell.sh
@@ -11,7 +11,7 @@ sudo systemctl disable snell.service
 sudo rm -f /etc/systemd/system/snell.service
 
 cd
-ARCHITECTURE=$(dpkg --print-architecture)
+ARCHITECTURE=$(uname -m)
 rm -f snell-server-v4.0.1-linux-$ARCHITECTURE.zip
 rm -f snell-server
 rm -f snell-server.conf
