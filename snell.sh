dd54a1319ea50f147c295c8c67f3f910e012edeb
Jian Qin <flounderqin@outlook.com>
2023-09-25 20:09:44 -0700

    Update snell.sh

--- snell.sh
+++ snell.sh
@@ -1,30 +1,40 @@
 #!/bin/bash
 
-if cat /etc/*-release | grep -q -E -i "debian|ubuntu|armbian|deepin|mint"; then 	# install dependencies
-	apt-get install wget unzip dpkg -y
-elif cat /etc/*-release | grep -q -E -i "centos|red hat|redhat"; then
-	yum install wget unzip dpkg -y
-elif cat /etc/*-release | grep -q -E -i "arch|Manjaro"; then
-	yes | pacman -S wget dpkg unzip
-elif cat /etc/*-release | grep -q -E -i "fedora"; then
-	dnf install wget unzip dpkg -y
+# Check if script is run as root and exit if it is
+if [ "$(id -u)" -eq 0 ]; then
+    echo "This script should not be run as root. Please run it as a regular user." 
+    exit 1
 fi
 
-echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf	# enable bbr
-echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-sysctl -p
-sysctl net.ipv4.tcp_available_congestion_control
+# Install dependencies based on the Linux distribution
+if cat /etc/*-release | grep -q -E -i "debian|ubuntu|armbian|deepin|mint"; then
+    sudo apt-get install wget unzip dpkg -y
+elif cat /etc/*-release | grep -q -E -i "centos|red hat|redhat"; then
+    sudo yum install wget unzip dpkg -y
+elif cat /etc/*-release | grep -q -E -i "arch|manjaro"; then
+    sudo pacman -S wget dpkg unzip --noconfirm
+elif cat /etc/*-release | grep -q -E -i "fedora"; then
+    sudo dnf install wget unzip dpkg -y
+fi
+
+# Enable BBR
+echo "net.core.default_qdisc=fq" | sudo tee -a /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
+sudo sysctl net.ipv4.tcp_available_congestion_control
 
 cd
 ARCHITECTURE=$(uname -m)
-wget -c https://dl.nssurge.com/snell/snell-server-v4.0.1-linux-$ARCHITECTURE.zip	# download binary
+wget -c https://dl.nssurge.com/snell/snell-server-v4.0.1-linux-$ARCHITECTURE.zip
 unzip -o snell-server-v4.0.1-linux-$ARCHITECTURE.zip
 
-echo -e "[Unit]\nDescription=snell server\n[Service]\nUser=root\nWorkingDirectory=/root\nExecStart=/root/snell-server\nRestart=always\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/snell.service
-y | ./snell-server
-systemctl start snell
-systemctl enable snell			# start service
+# Create systemd service
+echo -e "[Unit]\nDescription=snell server\n[Service]\nUser=root\nWorkingDirectory=/root\nExecStart=/root/snell-server\nRestart=always\n[Install]\nWantedBy=multi-user.target" | sudo tee /etc/systemd/system/snell.service > /dev/null
+echo "y" | sudo ./snell-server
+sudo systemctl start snell
+sudo systemctl enable snell
 
+# Print profile
 echo
-echo "Copy the following line to surge"			# print profile
+echo "Copy the following line to surge"
 echo "$(curl -s ipinfo.io/city) = snell, $(curl -s ipinfo.io/ip), $(cat snell-server.conf | grep -i listen | cut --delimiter=':' -f2),psk=$(grep 'psk' snell-server.conf | cut -d= -f2 | tr -d ' '), version=4, tfo=true"
