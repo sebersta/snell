41343c65ff7b03558d80a7b7f409817a16ae814e
Jian Qin <flounderqin@outlook.com>
2024-09-16 17:07:42 -0700

    Update rmsnell.sh

--- rmsnell.sh
+++ rmsnell.sh
@@ -5,6 +5,6 @@ sudo systemctl disable snell.service
 sudo rm -f /etc/systemd/system/snell.service
 
 cd
-rm -f rm snell-server-v4.0.1-linux-*
+rm -f rm snell-server-v4*
 rm -f snell-server
 rm -f snell-server.conf
